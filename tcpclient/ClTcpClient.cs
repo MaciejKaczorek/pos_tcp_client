71df051d1da3f8d307000836010bc096ad5b9506
MaciejKaczorek <38734831+MaciejKaczorek@users.noreply.github.com>
2018-06-12 13:38:37 +0200

    1.1
    Wprowadzono podział na klasy (dwie, jedna główna, jedna z logiką tcp klienta).

---
+++ tcpclient/ClTcpClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tcpclient
+{
+    class ClTcpClient
+    {
+        public static void client(string StrServAddr, int IntSocket)
+        {
+            byte[] data = new byte[1024];
+            string input, stringData;
+            TcpClient server;
+            try
+            {
+                server = new TcpClient(StrServAddr, IntSocket);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Unable to connect to server");
+                Console.ReadKey();
+                return;
+            }
+
+            NetworkStream ns = server.GetStream();
+            int recv = ns.Read(data, 0, data.Length);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine(stringData);
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+                ns.Write(Encoding.ASCII.GetBytes(input), 0, input.Length);
+                ns.Flush();
+                data = new byte[1024];
+                recv = ns.Read(data, 0, data.Length);
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine(stringData);
+            }
+            Console.WriteLine("Disconnecting from server...");
+            ns.Close();
+            server.Close();
+            Console.ReadKey();
+        }
+    }
+}
--- tcpclient/Program.cs
+++ tcpclient/Program.cs
@@ -13,44 +13,44 @@ namespace tcpclient
     {
 
 
-        public static void client(string StrServAddr, int IntSocket)
-        {
-            byte[] data = new byte[1024];
-            string input, stringData;
-            TcpClient server;
-            try
-            {
-                server = new TcpClient(StrServAddr, IntSocket);
-            }
-            catch (SocketException)
-            {
-                Console.WriteLine("Unable to connect to server");
-                Console.ReadKey();
-                return;
-            }
+        //public static void client(string StrServAddr, int IntSocket)
+        //{
+        //    byte[] data = new byte[1024];
+        //    string input, stringData;
+        //    TcpClient server;
+        //    try
+        //    {
+        //        server = new TcpClient(StrServAddr, IntSocket);
+        //    }
+        //    catch (SocketException)
+        //    {
+        //        Console.WriteLine("Unable to connect to server");
+        //        Console.ReadKey();
+        //        return;
+        //    }
 
-            NetworkStream ns = server.GetStream();
-            int recv = ns.Read(data, 0, data.Length);
-            stringData = Encoding.ASCII.GetString(data, 0, recv);
-            Console.WriteLine(stringData);
+        //    NetworkStream ns = server.GetStream();
+        //    int recv = ns.Read(data, 0, data.Length);
+        //    stringData = Encoding.ASCII.GetString(data, 0, recv);
+        //    Console.WriteLine(stringData);
 
-            while (true)
-            {
-                input = Console.ReadLine();
-                if (input == "exit")
-                    break;
-                ns.Write(Encoding.ASCII.GetBytes(input), 0, input.Length);
-                ns.Flush();
-                data = new byte[1024];
-                recv = ns.Read(data, 0, data.Length);
-                stringData = Encoding.ASCII.GetString(data, 0, recv);
-                Console.WriteLine(stringData);
-            }
-            Console.WriteLine("Disconnecting from server...");
-            ns.Close();
-            server.Close();
-            Console.ReadKey();
-        }
+        //    while (true)
+        //    {
+        //        input = Console.ReadLine();
+        //        if (input == "exit")
+        //            break;
+        //        ns.Write(Encoding.ASCII.GetBytes(input), 0, input.Length);
+        //        ns.Flush();
+        //        data = new byte[1024];
+        //        recv = ns.Read(data, 0, data.Length);
+        //        stringData = Encoding.ASCII.GetString(data, 0, recv);
+        //        Console.WriteLine(stringData);
+        //    }
+        //    Console.WriteLine("Disconnecting from server...");
+        //    ns.Close();
+        //    server.Close();
+        //    Console.ReadKey();
+        //}
         static void Main(string[] args)
         {
             String StrServAddr;
@@ -61,9 +61,9 @@ namespace tcpclient
             String StrSocket;
             StrSocket = Console.ReadLine();
             IntSocket = Convert.ToInt32(StrSocket);
-
-            Thread thr1=new Thread(client(StrServAddr, IntSocket));
-            
+            //client(StrServAddr, IntSocket);
+            //Thread thr1=new Thread(client(StrServAddr, IntSocket));
+            ClTcpClient.client(StrServAddr, IntSocket);
 
         }
 }
